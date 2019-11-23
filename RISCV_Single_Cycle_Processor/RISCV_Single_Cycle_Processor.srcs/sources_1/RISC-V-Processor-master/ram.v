9c8cfbd4c85ffd84b36f3af706f0449482a5193f
Jonathan Hall <37546166+JHall01@users.noreply.github.com>
2019-11-23 15:02:03 -0500

    Fetch, RAM, and Top modules implemented, needs checking
    Implemented the remaining modules and they simulate without error. Have not checked the test benches in depth, or expanded them. Need to check TOP module mostly, if it works. Added the muxes in the top module.

--- RISCV_Single_Cycle_Processor/RISCV_Single_Cycle_Processor.srcs/sources_1/RISC-V-Processor-master/ram.v
+++ RISCV_Single_Cycle_Processor/RISCV_Single_Cycle_Processor.srcs/sources_1/RISC-V-Processor-master/ram.v
@@ -20,12 +20,30 @@ module ram #(
 
 );
 
-localparam RAM_DEPTH = 1 << ADDR_WIDTH;
+localparam RAM_DEPTH = 1 << ADDR_WIDTH; // this code looks like it right shifts 1 by 16 places. Thus there are 2^16 addresses
 
 reg [DATA_WIDTH-1:0] ram [0:RAM_DEPTH-1];
 
 /******************************************************************************
 *                      Start Your Code Here
 ******************************************************************************/
+
+//first, the combinational read in the add
+//ask if this is ok, check it over.
+assign i_read_data = ram[i_address];
+
+assign d_read_data = ram[d_address];
+
+//now sequential write
+//this should be good
+always @(posedge clock)
+begin
+    if(wEn)
+    begin
+        ram[d_address] <= d_write_data;
+    end
+
+end
+
 
 endmodule
--- RISCV_Single_Cycle_Processor/RISCV_Single_Cycle_Processor.srcs/sources_1/RISC-V-Processor-master/top.v
+++ RISCV_Single_Cycle_Processor/RISCV_Single_Cycle_Processor.srcs/sources_1/RISC-V-Processor-master/top.v
@@ -17,28 +17,71 @@ module top #(
 ******************************************************************************/
 
 // Fetch Wires
+    // OUTPUT OF FETCH
+wire [ADDRESS_BITS-1:0] PC;
 
-// Decode Wires
+// Decode Wires ----------------------------------------------------------------------------------------------------------
+    //OUTPUT FROM DECODE
+wire next_PC_Select; //TO FETCH
+wire [ADDRESS_BITS-1:0] Target_PC; //TO FETCH
+wire [1:0] op_A_sel; //TO ALU MUX
+wire op_B_sel;
+wire [31:0] imm32;
+wire [5:0] ALU_Control;
 
 
-// Reg File Wires
-
+//To Reg File
+wire [4:0] read_Sel_1;
+wire [4:0] read_Sel_2;
+wire wEn;
+wire write_Sel;
+wire mem_wEn;
+wire branch_OP;
+// Reg File Wires -----------------------------------------------------------------
+wire read_Data_1; //TO ALU A MUX
+wire read_Data_2; //TO ALU B MUX
+//mem write back wires
+wire wb_Sel;
 // Execute Wires
 wire [ADDRESS_BITS-1:0] JALR_target; // Assigned outside of ALU
+wire [31:0] ALU_Res;
+wire branch;
+//wire for writeback to REG
+wire [31:0] reg_WB_Data;
+
+//OP A WIRE
+wire [31:0] OP_A_IN;
+wire [31:0] OP_B_IN;
 
 // Memory Wires
-
+   //OUTPUT FROM RAM - INSTRUCTION
+wire [31:0] Instruction;
+wire [31:0] d_Read_Data;
 // Writeback wires
 
+//mux for OP A and OP B - ask if in the right place
+//These values are input into ALU.
+assign OP_A_IN = (op_A_sel === 2'b00) ? read_Data_1:
+                 (op_A_sel === 2'b01) ? PC:
+                 (op_A_sel === 2'b10) ? PC+4:
+                 read_Data_1;
+assign OP_B_IN = (op_B_sel === 1'b0) ? read_Data_2:
+                 (op_B_sel === 1'b1) ? imm32:
+                 imm32;
+//assign mux to what is written back to REG
+assign reg_WB_Data = (wb_Sel === 1'b0) ? ALU_Res:
+                     (wb_Sel === 1'b1) ? d_Read_Data: //only from a load instruction.
+                      ALU_Res;
 
+//DONE but needs to be checked
 fetch #(
   .ADDRESS_BITS(ADDRESS_BITS)
 ) fetch_inst (
-  .clock(),
-  .reset(),
-  .next_PC_select(),
-  .target_PC(),
-  .PC()
+  .clock(clock),
+  .reset(reset),
+  .next_PC_select(next_PC_Select),
+  .target_PC(Target_PC),
+  .PC(PC)
 );
 
 
@@ -47,77 +90,78 @@ decode #(
 ) decode_unit (
 
   // Inputs from Fetch
-  .PC(),
-  .instruction(),
+  .PC(PC),
+  .instruction(Instruction),
 
   // Inputs from Execute/ALU
-  .JALR_target(),
-  .branch(),
+  .JALR_target(ALU_Res), // CHECK THIS
+  .branch(branch),
 
   // Outputs to Fetch
-  .next_PC_select(),
-  .target_PC(),
+  .next_PC_select(next_PC_Select),
+  .target_PC(Target_PC),
 
   // Outputs to Reg File
-  .read_sel1(),
-  .read_sel2(),
-  .write_sel(),
-  .wEn(),
+  .read_sel1(read_Sel_1),
+  .read_sel2(read_Sel_2),
+  .write_sel(write_Sel),
+  .wEn(wEn),
 
   // Outputs to Execute/ALU
-  .branch_op(),
-  .imm32(),
-  .op_A_sel(),
-  .op_B_sel(),
-  .ALU_Control(),
+  .branch_op(branch_OP),
+  .imm32(imm32),
+  .op_A_sel(op_A_sel),
+  .op_B_sel(op_B_sel),
+  .ALU_Control(ALU_Control),
 
   // Outputs to Memory
-  .mem_wEn(),
+  .mem_wEn(mem_wEn),
 
   // Outputs to Writeback
-  .wb_sel()
+  .wb_sel(wb_Sel)
 
 );
 
 
+//DONE but needs to be checked
 regFile regFile_inst (
-  .clock(),
-  .reset(),
-  .wEn(), // Write Enable
-  .write_data(),
-  .read_sel1(),
-  .read_sel2(),
-  .write_sel(),
-  .read_data1(),
-  .read_data2()
+  .clock(clock),
+  .reset(reset),
+  .wEn(wEn), // Write Enable
+  .write_data(reg_WB_data),
+  .read_sel1(read_Sel_1),
+  .read_sel2(read_Sel_2),
+  .write_sel(write_Sel),
+  .read_data1(read_Data_1),
+  .read_data2(read_Data_2)
 );
 
 
 
 ALU alu_inst(
-  .branch_op(),
-  .ALU_Control(),
-  .operand_A(),
-  .operand_B(),
-  .ALU_result(),
-  .branch()
+  .branch_op(branch_OP),
+  .ALU_Control(ALU_Control),
+  .operand_A(OP_A_IN),
+  .operand_B(OP_B_IN),
+  .ALU_result(ALU_Res),
+  .ALU_branch(branch)
 );
 
 
 ram #(
   .ADDR_WIDTH(ADDRESS_BITS)
 ) main_memory (
-  .clock(),
+  .clock(clock),
 
   // Instruction Port
-  .i_address(),
-  .i_read_data(),
+  .i_address(PC),
+  .i_read_data(Instruction),
 
   // Data Port
-  .wEn(),
-  .d_address(),
-  .d_write_data(),
-  .d_read_data()
+  .wEn(mem_wEn),
+  .d_address(ALU_Res),
+  .d_write_data(read_Data_2),
+  .d_read_data(d_Read_Data)
 );
 
 endmodule
